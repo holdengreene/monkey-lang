332167edd013c7fb4e76a535a48e0cbbc7cc1e32
Holden Greene <holdengreene26@gmail.com>
2024-08-12 22:00:54 -0600

    Init commit

---
+++ lexer/lexer.go
@@ -0,0 +1,31 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current postion in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
